223ddc90bed206d298cc1f5289b55fef0d3a6843
Michael Junek <michael@juneks.com.au>
2026-01-06 12:16:22 +1100

    Entrypoint cleanup

--- frontend/docker-entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# Replace placeholders with environment variables
-sed -i "s|PLACEHOLDER_API_URL|$API_URL|g" /var/www/html/services/api.js
-
-# Execute the main container command (e.g., starting Nginx)
-exec "$@"
