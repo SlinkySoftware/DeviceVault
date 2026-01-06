41a5836bc70c15d35c7495cc1d9d89eed89d51e7
Michael Junek <michael@juneks.com.au>
2026-01-06 14:38:49 +1100

    Fix issue in entrypoint

--- backend/docker-entrypoint.sh
+++ backend/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Run Django migrations
-python manage.py migrate --noinput -- run-syncdb
+python manage.py migrate --run-syncdb --noinput
 
 # Start the main Django application
 exec "$@"
