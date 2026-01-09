eea03508223add954b1fde71a8cda3ef275f93a7
Michael Junek <michael@juneks.com.au>
2026-01-09 15:41:23 +1100

    Allow additional aguments to worker

--- start-worker.sh
+++ start-worker.sh
@@ -10,4 +10,5 @@ celery -A celery_app worker \
     -Q default,collector.group.dmz_zone_queue,collector.group.secure_zone_queue \
     -l info \
     --concurrency=1 \
-    --max-tasks-per-child=100
+    --max-tasks-per-child=100 \
+    $*
